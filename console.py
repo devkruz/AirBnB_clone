357a7e6c5ac03d2e56707599493c57e6095eff3b
tonytec <tonyexplorer202@gmail.com>
2023-05-11 17:54:30 +0100

    Add all command to console

--- console.py
+++ console.py
@@ -60,6 +60,50 @@ class HBNBCommand(cmd.Cmd):
                     return
             print("* no instance found **")
 
+    def do_destroy(self, args):
+        """Deletes an instance"""
+        if args == "":
+            print("** class name missing **")
+            return
+        input = args.split()
+        class_name = input[0]
+        if class_name not in self.avaliable_class:
+            print("** class doesn't exist **")
+            return
+        try:
+            id = input[1]
+        except IndexError:
+            print("** instance id missing **")
+            return
+        else:
+            all_models = storage.all()
+            key_id = "{}.{}".format(class_name, id)
+            if key_id in all_models:
+                del all_models[key_id]
+                storage.save()
+                storage.reload()
+                print(storage.all())
+            else:
+                 print("** no instance found **")
+
+    def do_all(self, args):
+        """
+        Prints all string representation
+        of all instances or the specified one only
+        """
+        all_instances = storage.all().values()
+        if args == "":
+            print([str(obj) for obj in all_instances])
+            return
+        input = args.split()
+        class_name = input[0]
+        if class_name not in self.avaliable_class:
+            print("** class doesn't exist **")
+        else:
+            print([str(obj) for obj in all_instances
+                    if obj.__class__ == class_name])
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
