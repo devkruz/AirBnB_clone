2b8e532a7e5e2edf0de0a683bfb0fa00f28f6e84
tonytec <tonyexplorer202@gmail.com>
2023-05-11 11:56:22 +0100

    Add console.py file

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Airbnb Console"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Airbnb Console Commands"""
+    prompt = "(hbnb) "
+    intro = "Welcome to Airbnb Console Commands"
+
+    def do_EOF(self, args):
+        """Exit the programe"""
+        return True
+
+    def do_quit(self, args):
+        """Quit the programe"""
+        exit(1)
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
