ebff64cb87890333a22a86337714ddbf25b11371
tonytec <tonyexplorer202@gmail.com>
2023-05-14 12:43:54 +0100

    Update base_model file

--- main/main_1.py
+++ main/main_1.py
@@ -1,17 +1,25 @@
-import sibling_path as sibling_path
-from models import storage
+#!/usr/bin/python3
+import sibling_path
 from models.base_model import BaseModel
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-print(len(all_objs))
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new object --")
 my_model = BaseModel()
-my_model.name = "My_First_Base_Model"
+my_model.name = "My First Model"
 my_model.my_number = 89
-my_model.save()
+print("---First Model----")
 print(my_model)
+my_model.save()
+print("---First Model After Save----")
+print(my_model)
+my_model_json = my_model.to_dict()
+print("---First Model After to_dict----")
+print(my_model)
+print("---First Model to_dict copy----")
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+my_model_2 = BaseModel(**{"name": "Adetola", "age": 23, "id": 23})
+print(my_model_2)
+my_model_2_dic = my_model_2.to_dict()
+print(my_model_2_dic)
--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,7 @@ class BaseModel():
         from models import storage
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = self.updated_at = datetime.now()
             storage.new(self)
         else:
             for key, value in kwargs.items():
@@ -25,7 +24,7 @@ class BaseModel():
 
     def __str__(self):
         """Return BaseModel string representation"""
-        return "[{}] ({}) <{}>"\
+        return "[{}] ({}) {}"\
             .format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
@@ -39,9 +38,10 @@ class BaseModel():
         Returns a dictionary representation
         of instance
         """
-        dic = self.__dict__
+        dic = self.__dict__.copy()
         dic["__class__"] = type(self).__name__
-        dic["created_at"] = dic["created_at"].isoformat()
-        dic["updated_at"] = dic["updated_at"].isoformat()
+        for key in dic.keys():
+            if key in ("created_at", "updated_at"):
+                dic[key] = dic[key].isoformat()
 
         return dic
